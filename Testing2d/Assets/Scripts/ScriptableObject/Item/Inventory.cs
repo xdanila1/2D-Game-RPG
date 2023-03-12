44a44a9f5ce6183202d779fc4960578671909b32
xdanila1 <one.kekc@gmail.com>
2023-03-12 18:46:25 +0300

    The relationship between the store and inventory
    Также инвентарь отображает урон и стоимость предметов. Есть вероятность, что из-за множества ссылок у каждой ячейки предмета просядет фпс(тестировалось только около 50 предметов одновременно)

--- Testing2d/Assets/Scripts/ScriptableObject/Item/Inventory.cs
+++ Testing2d/Assets/Scripts/ScriptableObject/Item/Inventory.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using System;
+using UnityEngine.UI;
+using TMPro;
 
 public class Inventory : MonoBehaviour
 {
     [SerializeField] private List<AssetItem> Items;
     [SerializeField] private InventoryCell _ItemCellTemplate;
     [SerializeField] private Transform _Content;
+    [Space(10)]
+
+    [Header("UI FIELD INVENTORY")]
+    [SerializeField] private Image _iconItemInventory;
+    [SerializeField] private TextMeshProUGUI _damageItemInventory;
+    [SerializeField] private TextMeshProUGUI _priceItemInventory;
+    [SerializeField] private TextMeshProUGUI _nameItemInventory;
 
 
     private void OnEnable()
@@ -20,7 +29,7 @@ public class Inventory : MonoBehaviour
         items.ForEach(item =>
         {
             var cell = Instantiate(_ItemCellTemplate, _Content);
-            cell.Render(item);
+            cell.Render(item, _iconItemInventory, _nameItemInventory,_damageItemInventory, _priceItemInventory);
         });
     }
     public void AddItem(AssetItem item)
--- Testing2d/Assets/Scripts/ScriptableObject/Item/InventoryCell.cs
+++ Testing2d/Assets/Scripts/ScriptableObject/Item/InventoryCell.cs
@@ -1,16 +1,46 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-public class InventoryCell : MonoBehaviour
+using UnityEngine.EventSystems;
+public class InventoryCell : MonoBehaviour, IPointerEnterHandler
 {
+
     [SerializeField] private TextMeshProUGUI _nameField;
     [SerializeField] private Image _IconField;
 
+    private string _damageItem;
+    private string _priceItem;
 
-    public void Render(IItem item)
+    ///FIELD UI IN INVENTORY
+    private Image _iconInventory;
+    private TextMeshProUGUI _nameItemInventory;
+    private TextMeshProUGUI _damageItemInventory;
+    private TextMeshProUGUI _priceItemInventory;
+
+
+    public void Render(IItem item, Image IconFieldInventory, TextMeshProUGUI NameFieldInventory, TextMeshProUGUI DamageFieldInventory, TextMeshProUGUI PriceFieldInventory)
     {
         _nameField.text = item.Name;
+
+        if (item is Weapon weapon) _damageItem = weapon._A1.ToString();
+        else _damageItem = "-";
+        _priceItem = item.Price.ToString();
+
+        // Fill links in Fields UI
         _IconField.sprite = item.Icon;
+        _iconInventory = IconFieldInventory;
+        _damageItemInventory = DamageFieldInventory;
+        _priceItemInventory = PriceFieldInventory;
+        _nameItemInventory = NameFieldInventory;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _nameItemInventory.text = _nameField.text;
+        _iconInventory.sprite = _IconField.sprite;
+
+        _damageItemInventory.text = _damageItem;
+        _priceItemInventory.text = _priceItem;
     }
 }
 
