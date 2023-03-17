2832c0a2886c671694e13e85f2061c70737b64d3
xdanila1 <one.kekc@gmail.com>
2023-03-17 18:12:47 +0300

    Add relationship between hp and hpbarUI

--- Testing2d/Assets/Scripts/NPC/NPC.cs
+++ Testing2d/Assets/Scripts/NPC/NPC.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPC : MonoBehaviour, IDamagble
 {
@@ -8,8 +9,10 @@ public class NPC : MonoBehaviour, IDamagble
     private int _health=100;
     [SerializeField]
     private int _money = 1000;
-    
-    
+
+    [SerializeField] private Image _hpbar;
+
+
     public int Health { get => _health; set => _health = Mathf.Clamp(_health,0, MaxHP); }
     public int Money { get => _money; private set => _money = value; }
     [SerializeField] private Inventory Inventory;
@@ -17,6 +20,10 @@ public class NPC : MonoBehaviour, IDamagble
     [Range(100, 200)]
     public int MaxHP = 100;
 
+    private void Start()
+    {
+        _hpbar.fillAmount = _health / MaxHP;
+    }
 
     public void AddMoney(int num) => _money += num;
     public void TakeMoney(int num) => _money -= num;
@@ -28,10 +35,14 @@ public class NPC : MonoBehaviour, IDamagble
  
 
     private void OnValidate() => Health = _health;
-
+    private void Update()
+    {
+        _hpbar.fillAmount = (float)_health / (float)MaxHP;
+    }
     public void GetDamage(int damage)
     {
         _health -= damage;
+        _hpbar.fillAmount = _health / MaxHP;
         if (_health <= 0) Debug.Log("NPC умер");
     }
 }
